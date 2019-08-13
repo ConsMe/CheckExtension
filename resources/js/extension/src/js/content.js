b240d632b686d347d8dce0aefedcd9b3c5a9c660
Alex M <alexandr.makarov@balance-pl.ru>
2019-08-13 15:36:17 +0300

    next step

--- resources/js/extension/src/js/content.js
+++ resources/js/extension/src/js/content.js
@@ -1,6 +1,8 @@
 let $ = require('jquery')
-
+console.log(1)
 chrome.runtime.onConnect.addListener(function(port) {
+    console.log(2)
+    port.postMessage({m: 'hello'})
     port.onMessage.addListener(function(msg) {
         console.log(msg)
         if (!msg.pong) return
